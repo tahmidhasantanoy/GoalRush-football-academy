8d961a2948ed6726528eb40a9dd9ede1a1d10a3f
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-06-10 20:53:38 +0600

    Class adding function complete

--- src/Pages/AddClass/AddClass.jsx
+++ src/Pages/AddClass/AddClass.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useAuth from "../../Hooks/useAuth";
 import { useForm } from "react-hook-form";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const AddClass = () => {
   const {
@@ -10,6 +11,8 @@ const AddClass = () => {
     formState: { errors },
   } = useForm();
   const { user, loading } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+  console.log(user);
 
   const img_hosting_token = import.meta.env.VITE_Image_Hosting_Token;
 
@@ -17,6 +20,7 @@ const AddClass = () => {
 
   const onSubmit = (data) => {
     console.log(data.file[0]);
+    console.log(data);
 
     //Hosting image yo imgbb
     if (data.file[0]) {
@@ -28,7 +32,33 @@ const AddClass = () => {
         body: formData,
       })
         .then((res) => res.json())
-        .then((data) => console.log(data))
+        .then((imgRes) => {
+          if (imgRes.success) {
+            console.log(data);
+            console.log(imgRes.data.display_url);
+            const imgURL = imgRes.data.display_url;
+            const {
+              classname,
+              instructorEmail,
+              instructorName,
+              availableSeats,
+              price,
+            } = data;
+
+            const newClassData = {
+              classname,
+              instructorName,
+              instructorEmail,
+              availableSeats: parseInt(availableSeats),
+              price: parseFloat(price),
+              image: imgURL,
+            };
+
+            axiosSecure.post("/all-class", newClassData).then((res) => {
+              console.log(res.data);
+            });
+          }
+        })
         .catch((err) => console.log(err.message));
     }
   };
@@ -62,7 +92,7 @@ const AddClass = () => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Enter the class name"
             required
-            defaultValue={user.displayName}
+            defaultValue={user?.displayName}
             // defaultValue={user?.DisplayName}
             readOnly
           />
@@ -78,7 +108,7 @@ const AddClass = () => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="goalrush@gmail.com"
             required
-            defaultValue={user.email}
+            defaultValue={user?.email}
             // defaultValue={user?.DisplayName}
             readOnly
           />
