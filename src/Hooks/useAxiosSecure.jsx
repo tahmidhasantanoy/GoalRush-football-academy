84d67bb49a71eb4748dfd53e1a9eb15d9e90becd
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-06-10 20:11:42 +0600

    useAxiosSecure hook is created

---
+++ src/Hooks/useAxiosSecure.jsx
@@ -0,0 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
+import axios from "axios";
+import { useEffect } from "react";
+
+const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  //Base url for all
+  const axiosSecure = axios.create({
+    baseURL: "http://localhost:5000",
+  });
+
+  //Jwt header is here & use all over
+  useEffect(() => {
+    axiosSecure.interceptors.request.use((config) => {
+      const token = localStorage.getItem("Jwt-access-token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+
+    axiosSecure.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (
+          error.response &&
+          (error.response.status === 401 || error.response.status === 403)
+        ) {
+          await logout();
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [logout, navigate, axiosSecure]);
+
+  return [axiosSecure];
+};
+
+export default useAxiosSecure;
