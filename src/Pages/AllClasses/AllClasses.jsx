820ef8da40c744384697b4afe2c78ebf20f8103c
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-06-12 10:06:42 +0600

    Class page is almost ready

--- src/Pages/AllClasses/AllClasses.jsx
+++ src/Pages/AllClasses/AllClasses.jsx
@@ -17,18 +17,21 @@ const AllClasses = () => {
   return (
     <div className="grid gri md:grid-cols-2 pt-28">
       {classData.map((item) => (
-        <div className="card card-compact w-96 bg-teal-950 shadow-2xl">
+        <div className="card card-compact w-96 bg-[#1a4949] shadow-2xl hover:scale-105 duration-1000 mb-8">
+          <p className="mt-5 mb-1 pl-5 text-white font-semibold text-2xl">
+            {item?.classname}
+          </p>
           <figure>
-            <img
-              src={item?.image}
-              alt="Shoes"
-            />
+            <img className="p-3 rounded-3xl" src={item?.image} alt="Shoes" />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">{item?.name}</h2>
-            <p>{item?.instructorName}</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+          <div className="card-body space-y-2 ml-3">
+            <p className="text-2xl">{item?.instructorName}</p>
+            <p className="border-b-2 mx-auto ml-0">Available Now : {item?.availableSeats} seats</p>
+            <p className="bg-white mx-auto ml-0 p-1 px-3 rounded-2xl text-black font-semibold">
+              ${item?.price}
+            </p>
+            <div className="card-actions justify-start">
+            <button className="btn btn-sm w-full mt-5 mx-auto">Book now</button>
             </div>
           </div>
         </div>
