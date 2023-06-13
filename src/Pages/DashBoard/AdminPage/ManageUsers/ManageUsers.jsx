237e4e9efa4499558804737bd70f35d822863358
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-06-13 16:05:35 +0600

    all user Show in admin dashboard

--- src/Pages/DashBoard/AdminPage/ManageUsers/ManageUsers.jsx
+++ src/Pages/DashBoard/AdminPage/ManageUsers/ManageUsers.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import useAlluser from "../../../../Hooks/useAlluser";
-import { FaUserAstronaut, FaUserEdit, FaUserSecret, FaUserTie } from "react-icons/fa";
+import {
+  FaUserAstronaut,
+  FaUserEdit,
+  FaUserSecret,
+  FaUserTie,
+} from "react-icons/fa";
 
 const ManageUsers = () => {
   const [usersData] = useAlluser();
@@ -23,13 +28,38 @@ const ManageUsers = () => {
             </tr>
           </thead>
           {usersData.map((user, idx) => (
-            <tbody key={user._id}>
+            <tbody>
+              {/* row 1 */}
               <tr>
-                <th>{idx + 1}</th>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td><FaUserEdit className="w-40 pr-16"/></td>
-                <td><FaUserAstronaut className="w-40 pr-20"/></td>
+                <th>
+                  <label>{idx + 1}</label>
+                </th>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={user.image}
+                          alt="No image found"
+                        />
+                      </div>
+                    </div>
+                    <div></div>
+                  </div>
+                </td>
+                <td>
+                  <span className="ml-2">{user.name}</span>
+                  <br />
+                  <span className="badge badge-ghost badge-sm ml-0">
+                    {user.email}
+                  </span>
+                </td>
+                <td>
+                  <FaUserEdit className="w-40 pr-16" />
+                </td>
+                <td>
+                  <FaUserAstronaut className="w-40 pr-20" />
+                </td>
               </tr>
             </tbody>
           ))}
