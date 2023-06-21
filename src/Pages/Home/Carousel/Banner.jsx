c6f8733a0263bf4dc335b8fc724f1949d7639191
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-06-21 19:42:20 +0600

    Swap carousel modified done

--- src/Pages/Home/Carousel/Banner.jsx
+++ src/Pages/Home/Carousel/Banner.jsx
@@ -1,51 +1,182 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
+import "./Banner.css";
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
+import withAutoplay from "react-awesome-slider/dist/autoplay";
+import "react-awesome-slider/dist/styles.css";
 
 import Pic1 from "../../../../public/Banner/ban11.jpeg";
 import Pic2 from "../../../../public/Banner/ban2.jpeg";
 import Pic3 from "../../../../public/Banner/ban3.jpeg";
 import Pic4 from "../../../../public/Banner/ban1.jpeg";
-import Pic5 from "../../../../public/Banner/ban6.jpeg"
+import Pic5 from "../../../../public/Banner/ban6.jpeg";
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 const Banner = () => {
   return (
-    <div>
-      <Carousel autoPlay infiniteLoop className="text-center">
-        <div>
-        <div
-            className="hero min-h-screen"
-            style={{backgroundImage : `url(${Pic1})`}}
+    <>
+      <div>
+        <AutoplaySlider
+          play={true}
+          cancelOnInteraction={false} // should stop playing on user interaction
+          interval={6000}
+        >
+          <div
+            className="hero min-h-screen rounded-xl"
+            style={{ backgroundImage: `url(${Pic1})`, alignItems: "normal" }}
           >
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-              <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                <p className="mb-5">
+            <div className="hero-overlay bg-opacity-5 image-gradient"></div>
+            <div className=" text-start">
+              <div className="max-w-5xl sm:ml-12">
+                <h1 className="mb-5 text-3xl font-bold text-white">
+                  Join with <span className="text-info">GoalRush</span> <br />{" "}
+                  Enjoy Football In Summer
+                </h1>
+                <p className="mb-5 w-4/6 text-md">
                   Provident cupiditate voluptatem et in. Quaerat fugiat ut
                   assumenda excepturi exercitationem quasi. In deleniti eaque
                   aut repudiandae et a id nisi.
                 </p>
-                <button className="btn btn-primary">Get Started</button>
+                <button className="btn btn-primary text-white hover:text-black hover:bg-cyan-600">
+                  Get Started
+                </button>
               </div>
             </div>
           </div>
-          {/* <img src={Pic1} /> */}
-        </div>
-        <div>
-          <img src={Pic2} />
-        </div>
-        <div>
-          <img src={Pic3} />
-        </div>
-        <div>
-          <img src={Pic4} />
-        </div>
-        <div>
-          <img src={Pic5} />
-        </div>
-      </Carousel>
-    </div>
+          <div
+            className="hero min-h-screen rounded-xl"
+            style={{ backgroundImage: `url(${Pic2})`, alignItems: "normal" }}
+          >
+            <div className="hero-overlay bg-opacity-5 image-gradient"></div>
+            <div className=" text-start">
+              <div className="max-w-5xl sm:ml-12">
+                <h1 className="mb-5 text-3xl font-bold text-white">
+                  Join with <span className="text-info">GoalRush</span> <br />{" "}
+                  Enjoy Football In Summer
+                </h1>
+                <p className="mb-5 w-4/6 text-md">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                  assumenda excepturi exercitationem quasi. In deleniti eaque
+                  aut repudiandae et a id nisi.
+                </p>
+                <button className="btn btn-primary text-white hover:text-black hover:bg-cyan-600">
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            className="hero min-h-screen rounded-xl"
+            style={{ backgroundImage: `url(${Pic3})`, alignItems: "normal" }}
+          >
+            <div className="hero-overlay bg-opacity-5 image-gradient"></div>
+            <div className=" text-start">
+              <div className="max-w-5xl sm:ml-12">
+                <h1 className="mb-5 text-3xl font-bold text-white">
+                  Join with <span className="text-info">GoalRush</span> <br />{" "}
+                  Enjoy Football In Summer
+                </h1>
+                <p className="mb-5 w-4/6 text-md">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                  assumenda excepturi exercitationem quasi. In deleniti eaque
+                  aut repudiandae et a id nisi.
+                </p>
+                <button className="btn btn-primary text-white hover:text-black hover:bg-cyan-600">
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            className="hero min-h-screen rounded-xl"
+            style={{ backgroundImage: `url(${Pic4})`, alignItems: "normal" }}
+          >
+            <div className="hero-overlay bg-opacity-5 image-gradient"></div>
+            <div className=" text-start">
+              <div className="max-w-5xl sm:ml-12">
+                <h1 className="mb-5 text-3xl font-bold text-white">
+                  Join with <span className="text-info">GoalRush</span> <br />{" "}
+                  Enjoy Football In Summer
+                </h1>
+                <p className="mb-5 w-4/6 text-md">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                  assumenda excepturi exercitationem quasi. In deleniti eaque
+                  aut repudiandae et a id nisi.
+                </p>
+                <button className="btn btn-primary text-white hover:text-black hover:bg-cyan-600">
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            className="hero min-h-screen rounded-xl"
+            style={{ backgroundImage: `url(${Pic5})`, alignItems: "normal" }}
+          >
+            <div className="hero-overlay bg-opacity-5 image-gradient"></div>
+            <div className=" text-start">
+              <div className="max-w-5xl sm:ml-12">
+                <h1 className="mb-5 text-3xl font-bold text-white">
+                  Join with <span className="text-info">GoalRush</span> <br />{" "}
+                  Enjoy Football In Summer
+                </h1>
+                <p className="mb-5 w-4/6 text-md">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                  assumenda excepturi exercitationem quasi. In deleniti eaque
+                  aut repudiandae et a id nisi.
+                </p>
+                <button className="btn btn-primary text-white hover:text-black hover:bg-cyan-600">
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+        </AutoplaySlider>
+      </div>
+
+      <div>
+        {/* <Carousel autoPlay infiniteLoop className="text-center">
+          <div>
+            <div
+              className="hero min-h-screen rounded-xl"
+              style={{ backgroundImage: `url(${Pic1})` }}
+            >
+              <div className="hero-overlay bg-opacity-5 gradient"></div>
+              <div className=" text-start">
+                <div className="max-w-5xl sm:ml-12">
+                  <h1 className="mb-5 text-3xl font-bold text-white">
+                    Join with <span className="text-info">GoalRush</span> <br />{" "}
+                    Enjoy Football In Summer
+                  </h1>
+                  <p className="mb-5 w-4/6 text-md">
+                    Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                    assumenda excepturi exercitationem quasi. In deleniti eaque
+                    aut repudiandae et a id nisi.
+                  </p>
+                  <button className="btn btn-primary text-white hover:text-black hover:bg-cyan-600">
+                    Get Started
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={Pic2} />
+          </div>
+          <div>
+            <img src={Pic3} />
+          </div>
+          <div>
+            <img src={Pic4} />
+          </div>
+          <div>
+            <img src={Pic5} />
+          </div>
+        </Carousel> */}
+      </div>
+    </>
   );
 };
 
