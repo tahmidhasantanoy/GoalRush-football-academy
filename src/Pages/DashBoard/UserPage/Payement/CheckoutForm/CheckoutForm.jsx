b5c81f821cd9eae6bc1a336b6453484bdb6749e6
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2024-09-01 23:26:28 +0600

    Dashboard in payment design problem solved

--- src/Pages/DashBoard/UserPage/Payement/CheckoutForm/CheckoutForm.jsx
+++ src/Pages/DashBoard/UserPage/Payement/CheckoutForm/CheckoutForm.jsx
@@ -102,12 +102,13 @@ const CheckoutForm = ({ paymentClassData, price }) => {
       });
     }
   };
+
   return (
-    <>
+    <div className="">
       <div className="flex justify-center">
-        <form onSubmit={handleSubmit} className="">
+        <form onSubmit={handleSubmit} >
           <CardElement
-            className="w-[400px] md:w-[500px]"
+            className="w-80 md:w-96"
             options={{
               style: {
                 base: {
@@ -123,6 +124,13 @@ const CheckoutForm = ({ paymentClassData, price }) => {
               },
             }}
           />
+          {CardErr && <p className="text-error">{CardErr}</p>}
+          {transsactionId && (
+            <p className="text-success">
+              Payment Success with{" "}
+              <span className="text-error">{transsactionId}</span>
+            </p>
+          )}
           <button
             className="btn-primary normal-case"
             type="submit"
@@ -132,31 +140,9 @@ const CheckoutForm = ({ paymentClassData, price }) => {
           </button>
         </form>
       </div>
-      <div className="">
-        {CardErr && <p className="text-error">{CardErr}</p>}
-        {transsactionId && (
-          <p className="text-success">
-            Payment Success with{" "}
-            <span className="text-error">{transsactionId}</span>
-          </p>
-        )}
-      </div>
 
-      <div className="bg-red-400 text-black flex flex-col space-y-4 justify-center items-center w-[770px] h-[250px] rounded-2xl border-2 border-blue-300 my-10">
-        <p className="text-zinc-700 text-3xl underline">
-          Please give a valid card number & date
-        </p>
-        <p>
-          card number : <span className="text-white">4242 4242 4242 4242</span>
-        </p>
-        <p>
-          Month : <span className="text-white">12</span> Year :{" "}
-          <span className="text-white">33</span> CVC :{" "}
-          <span className="text-white"> 123</span> ZIP :{" "}
-          <span className="text-white">12345</span>
-        </p>
-      </div>
-    </>
+      
+    </div>
   );
 };
 
--- src/Pages/DashBoard/UserPage/Payement/PaymentPage/PaymentPage.jsx
+++ src/Pages/DashBoard/UserPage/Payement/PaymentPage/PaymentPage.jsx
@@ -16,7 +16,7 @@ const PaymentPage = () => {
   const price = parseFloat(classPrice.toFixed(2));
 
   return (
-    <div className=".my-10">
+    <div className="">
       <p className="text-black text-2xl md:text-4xl text-center">
         Pay for selected class
       </p>
@@ -30,6 +30,28 @@ const PaymentPage = () => {
             price={price}
           ></CheckoutForm>
         </Elements>
+      </div>
+
+
+      <div className="flex justify-center ">
+        <div
+          className="bg-[#1F316F] text-black flex flex-col space-y-4 justify-center items-center rounded-2xl border-2 border-blue-300 py-8 px-8 mt-10 my-6 z-50 relative"
+          style={{ zIndex: 50 }}
+        >
+          <p className="text-[#F9DBBA]  bg-[#5B99C2] px-4 py-2 rounded-2xl text-3xl">
+            provide valid information
+          </p>
+          <p>
+            card number :{" "}
+            <span className="text-white">4242 4242 4242 4242</span>
+          </p>
+          <p>
+            Month : <span className="text-white">12</span> Year :{" "}
+            <span className="text-white">33</span> CVC :{" "}
+            <span className="text-white"> 123</span> ZIP :{" "}
+            <span className="text-white">12345</span>
+          </p>
+        </div>
       </div>
     </div>
   );
