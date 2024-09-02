0ae4f320444b7eca435708c11881cff032552d5a
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2024-09-02 19:27:31 +0600

    Search item  added

--- src/Pages/DashBoard/AdminPage/ManageClass/ManageClass.jsx
+++ src/Pages/DashBoard/AdminPage/ManageClass/ManageClass.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import useAllClass from "../../../../Hooks/useAllClass";
-import { FaCaretRight, FaPlusCircle } from "react-icons/fa";
+import { FaCaretRight, FaPlusCircle, FaSearch } from "react-icons/fa";
 import Swal from "sweetalert2";
 import { Fade } from "react-awesome-reveal";
 import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
 import TitlePage from "../../../../TitlePage/TitlePage";
 import SeeMoreButton from "../../../../components/ui/SeeMoreButton/SeeMoreButton";
+import { IoMdClose } from "react-icons/io";
 
 const ManageClass = () => {
   const [classData, refetch] = useAllClass();
   const [feedback, setFeedback] = useState(false);
   const [id, setId] = useState("");
   const [axiosSecure] = useAxiosSecure();
+  const [filterItem, setFilterItem] = useState("");
   const [visibleItem, setVisibleItem] = useState(6);
   // console.log(classData);
 
@@ -113,11 +115,20 @@ const ManageClass = () => {
     );
   }
 
+  const searchItems = classData?.filter(
+    (classItem) =>
+      !filterItem ||
+      classItem?.classname?.toLowerCase().includes(filterItem.toLowerCase())
+  );
+
+  const handleClear = () => setFilterItem("");
+
   const handleSeeMore = () => setVisibleItem((prevItem) => prevItem + 6);
 
   return (
     <div className="mt-28 mb-20">
       <TitlePage title={"Manage Class | Dashboard"}></TitlePage>
+
       <Fade cascade damping={0.1}>
         <p className="text-center text-black text-2xl font-semibold">
           All Classes of GoalRush
@@ -125,13 +136,42 @@ const ManageClass = () => {
       </Fade>
 
       <hr className="w-1/2 mx-auto" />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
-        {classData.slice(0, visibleItem).map((data) => (
+
+      <div className="flex justify-center items-center m-0 p-0">
+        <div className="relative w-2/5 mt-5">
+          <input
+            value={filterItem}
+            onChange={(e) => setFilterItem(e.target.value)}
+            className="w-full py-3 px-4 pl-10 border border-gray-300 rounded-3xl shadow-lg hover:shadow-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-300"
+            type="text"
+            placeholder="Search for classes..."
+          />
+          {filterItem && (
+            <p className="text-center font-bold">
+              Search for : <span className="text-error">{filterItem}</span>
+            </p>
+          )}
+          <button className="absolute btn-outline left-0 top-[8px] px-4 flex items-cente text-white rounded-l-lg hover:bg-white hover:bg-opacity-10 hover:shadow-none focus:outline-none transition-all duration-300">
+            <FaSearch className="w-4 h-8 btn-outline hover:btn-outline hover:bg-transparent transition-all duration-300" />
+          </button>
+
+          {filterItem && (
+            <button
+              onClick={handleClear}
+              className="absolute right-2 top-2 btn-outline hover:rounded-full text-gray-500 hover:text-black hover:bg-white focus:outline-none transition-all duration-300"
+            >
+              <IoMdClose className="w-5 h-5" />
+            </button>
+          )}
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2">
+        {searchItems.slice(0, visibleItem).map((data) => (
           <div
             key={data._id}
-            className="rounded overflow-hidden shadow-xl bg-base-100 hover:bg-base-300 duration-500 border-blue-800 border-2 classCard px-8 pt-8 pb-4 m-12"
+            className="rounded overflow-hidden shadow-xl bg-base-100 hover:bg-base-300 duration-500 border-blue-800 border-2 px-2 md:px-4 lg:px-6 xl:px-8 pt-8 pb-4 m-12 classCard"
           >
-            <img className="h-[200px] w-[500px]" src={data.image} alt="River" />
+            <img className="h-[200px] w-[500px]" src={data.image} alt="class image" />
             <div className="px-6">
               <p className="font-bold text-xl mt-4">{data.classname}</p>
             </div>
